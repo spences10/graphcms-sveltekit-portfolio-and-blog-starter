b7a2fe227e8042939f680943b49f2896b17c8c75
spences10 <spences10apps@gmail.com>
2022-02-24 17:37:57 +0000

    refactor: :recycle: aliases in vite config added

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
-import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-auto'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          '@components': path.resolve('./src/lib/components'),
+          '@lib': path.resolve('./src/lib'),
+          '@utils': path.resolve('./src/lib/utils'),
+        },
+      },
+    },
   },
 
   preprocess: [
